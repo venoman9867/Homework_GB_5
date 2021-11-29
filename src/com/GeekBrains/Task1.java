4b072a2f70f55165c4c7d01a2e109c034a66b89a
ALEX <41444141+venoman9867@users.noreply.github.com>
2021-11-29 10:48:28 +0300

    commit

--- src/com/GeekBrains/Task1.java
+++ src/com/GeekBrains/Task1.java
@@ -22,13 +22,19 @@ public class Task1 extends Thread{
         }
        long startTime = System.currentTimeMillis();
         int[] left=new int[h];
+        for(int j=0;j<left.length;j++){
+            left[j] = (int)(left[j] * Math.sin(0.2f + j / 5) * Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
+        }
         int[] right=new int[h];
+        for(int j1=0;j1<right.length;j1++){
+            right[j1] = (int)(right[j1] * Math.sin(0.2f + j1 / 5) * Math.cos(0.2f + j1 / 5) * Math.cos(0.4f + j1 / 2));
+        }
         System.arraycopy(array,0,left,0,h);
         System.arraycopy(array,h,right,0,h);
         int[] mergedArray=new int[size];
         System.arraycopy(left, 0, mergedArray, 0, h);
         System.arraycopy(right, 0, mergedArray, h, h);
 
-        System.out.println("поток 1 "+(System.currentTimeMillis()-startTime)+" ms");
+        System.out.println("поток 2 "+(System.currentTimeMillis()-startTime)+" ms");
     }
 }
